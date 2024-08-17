e6b2f656b4ef389b6c460c74100d89eb9de67dfe
omar magdy <omarmagdyzz12@gmail.com>
2024-08-17 20:04:47 +0300

    finish the second task

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -10,21 +10,21 @@ function Counter() {
           setCount(count + 1);
         }}
       >
-        increment
+        Increment
       </button>
       <button
         onClick={() => {
           setCount(count - 1);
         }}
       >
-        decrement
+        Decrement
       </button>
       <button
         onClick={() => {
           setCount(0);
         }}
       >
-        reset
+        Reset
       </button>
     </>
   );
