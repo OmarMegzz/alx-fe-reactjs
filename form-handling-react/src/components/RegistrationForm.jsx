6a7659db6d730bcde63ec7aae347dbbdf0158c5b
omar magdy <omarmagdyzz12@gmail.com>
2024-08-28 17:53:25 +0300

    finish first task

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -8,34 +8,75 @@ const RegistrationForm = () => {
     password: "",
   });
 
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    const newErrors = {};
+    if (!username) newErrors.username = "user name is required";
+
+    if (!email) {
+      newErrors.email = "email is required";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      newErrors.email = "Email is invalid";
+    }
+
+    if (!password) newErrors.password = "password is required";
+
+    return newErrors;
+  };
+
   const { username, email, password } = formData;
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevstate) => ({ ...prevstate, [name]: value }));
+    setErrors((prevErrors) => ({ ...prevErrors, [name]: "" }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    const validationErrors = validateForm();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      console.log(formData);
+    }
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          gap: "10px",
+        }}
+        onSubmit={handleSubmit}
+      >
         <input
           type="text"
           name="username"
           value={username}
           onChange={handleChange}
         />
+        {errors.username && (
+          <span style={{ color: "red" }}>{errors.username}</span>
+        )}
         <input type="text" name="email" value={email} onChange={handleChange} />
+        {errors.email && (
+          <span style={{ color: "red" }}>{errors.username}</span>
+        )}
         <input
           type="text"
           name="password"
           value={password}
           onChange={handleChange}
         />
+        {errors.password && (
+          <span style={{ color: "red" }}>{errors.username}</span>
+        )}
         <button type="submit">submit</button>
       </form>
     </>
