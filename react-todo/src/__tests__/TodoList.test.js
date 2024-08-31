42592d982fa7d7d281d20bb2c4009a4e2f86976d
omar magdy <omarmagdyzz12@gmail.com>
2024-08-31 19:16:49 +0300

    finish the last task

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -6,46 +6,31 @@ import TodoList from "../TodoList";
 
 test("renders TodoList component with initial todos", () => {
   render(<TodoList />);
-
-  // Check if initial todos are rendered
   expect(screen.getByText(/Learn React/i)).toBeInTheDocument();
   expect(screen.getByText(/Learn Jest/i)).toBeInTheDocument();
 });
 
 test("can add a new todo", () => {
   render(<TodoList />);
-
-  // Simulate adding a new todo
   fireEvent.change(screen.getByRole("textbox"), {
     target: { value: "New Todo" },
   });
   fireEvent.click(screen.getByText(/Add Todo/i));
-
-  // Verify new todo is added
   expect(screen.getByText(/New Todo/i)).toBeInTheDocument();
 });
 
 test("can toggle a todo", () => {
   render(<TodoList />);
-
-  // Find the todo item
   const todoItem = screen.getByText(/Learn React/i);
-
-  // Toggle the todo item
   fireEvent.click(todoItem);
   expect(todoItem).toHaveStyle("text-decoration: line-through");
-
   fireEvent.click(todoItem);
   expect(todoItem).toHaveStyle("text-decoration: none");
 });
 
 test("can delete a todo", () => {
   render(<TodoList />);
-
-  // Find and click the delete button
   const deleteButton = screen.getAllByText(/Delete/i)[0];
   fireEvent.click(deleteButton);
-
-  // Verify the todo item is deleted
   expect(screen.queryByText(/Learn React/i)).toBeNull();
 });
