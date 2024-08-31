3ddf72bc4f604448b3331909e37a714713f598c1
omar magdy <omarmagdyzz12@gmail.com>
2024-08-31 15:50:13 +0300

    the api func and display done

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import "./App.css";
-import PostsComponent from "./PostsComponent";
+import PostsComponent from "./components/PostsComponent";
 
 const queryClient = new QueryClient();
 
