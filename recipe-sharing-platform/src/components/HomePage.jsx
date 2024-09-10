042228d1216a0297f5aa1f852090f1daab12e35d
omar magdy <omarmagdyzz12@gmail.com>
2024-09-10 15:46:09 +0300

    finish second task

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -18,7 +18,7 @@ function HomePage() {
   return (
     <>
       <div className=" max-w-7xl mx-auto p-4 ">
-        <ul className="flex flex-wrap gap-6 justify-center  ">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6  ">
           {recipes.map((recipe) => (
             <li
               className="flex flex-col bg-white shadow-lg rounded-lg overflow-hidden max-w-sm w-full transition-transform transform hover:scale-105"
